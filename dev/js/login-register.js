f029cb3ef33518e2247637801f5bcc13c2bad03f
wguevarc <wguevarac85@gmail.com>
2021-11-02 23:23:45 -0500

    login_2

--- dev/js/login-register.js
+++ dev/js/login-register.js
@@ -1,6 +1,6 @@
 const topbar = document.getElementById('topbar')
 
-const loginModal = document.getElementById('login-topbar')
+const loginModal = document.getElementById('login-modal')
 const registerModal = document.getElementById('register-modal')
 
 const loginForm = document.getElementById('login-form')
@@ -8,9 +8,25 @@ const registerform = document.getElementById('register-form')
 
 if (topbar){
     topbar.addEventListener('click',(e)=>{
-        if(e.target.parentElement.dataset.type |= undefined){
+        if(e.target.parentElement.dataset.type != undefined){
+            if (e.target.parentElement.dataset.type == 'login'){
             loginModal.classList.add('lighbox--show')
+            }
 
         }
+    })
+}
+if(loginModal){
+    loginModal.addEventListener('click',(e) => {
+        if (e.target.classList.contains('lighbox')){
+            loginModal.classList.remove('lighbox--show')
+        }
+
+    })
+}
+
+if(loginForm){
+    loginForm.addEventListener('submit',(e) => {
+        e.preventDefault()
     })
 }
