7e4f10ab77b6c5c6114461179615f3ffd0bbf13a
wguevarc <wguevarac85@gmail.com>
2021-11-03 22:50:27 -0500

    modalregistre

--- dev/js/login-register.js
+++ dev/js/login-register.js
@@ -6,16 +6,18 @@ const registerModal = document.getElementById('register-modal')
 const loginForm = document.getElementById('login-form')
 const registerform = document.getElementById('register-form')
 
-if (topbar){
+if (topbar)
     topbar.addEventListener('click',(e)=>{
         if(e.target.parentElement.dataset.type != undefined){
             if (e.target.parentElement.dataset.type == 'login'){
             loginModal.classList.add('lighbox--show')
+            } else  if (e.target.parentElement.dataset.type == 'register'){
+                registerModal.classList.add('lighbox--show')
             }
 
         }
     })
-}
+
 if(loginModal){
     loginModal.addEventListener('click',(e) => {
         if (e.target.classList.contains('lighbox')){
@@ -25,8 +27,31 @@ if(loginModal){
     })
 }
 
+if(registerModal){
+    registerModal.addEventListener('click',(e)=>{
+        if (e.target.classList.contains('lighbox')){
+            registerModal.classList.remove('lighbox--show')
+        }
+    })
+}
+
+
 if(loginForm){
     loginForm.addEventListener('submit',(e) => {
         e.preventDefault()
+        setTimeout(() => {
+            e.target.button.blur()
+        }, 200);        
+        console.log('SEND');
+    })
+}
+
+if(registerform){
+    registerform.addEventListener('submit',(e)=>{
+        e.preventDefault()
+        setTimeout(() => {
+            e.target.button.blur()
+        }, 200);        
+        console.log('SEND');
     })
 }
